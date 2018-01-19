63e46d58621e00cc79a7bcc17130f03adf175b9d
TheArnabDey <arnab.pgpm16c@greatlakes.edu.in>
2018-01-19 23:20:41 +0530

    Add files via upload

---
+++ Open.py
@@ -0,0 +1,118 @@
+### Quantiacs Trend Following Trading System Example
+# import necessary Packages below:
+import numpy
+import pandas as pd
+def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, exposure, equity, settings):
+    ''' This system uses trend following techniques to allocate capital into the desired equities'''
+    oos = settings['markets']
+    #print(DATE)
+    #print(oos)
+    print(pd.DataFrame(OPEN))
+    df = pd.DataFrame(OPEN, index=DATE, columns=oos)
+    df['DATE'] = df.index
+    try:
+        da = pd.read_csv('Open.csv')
+        df = da.append(df)
+        df.to_csv('Open.csv',index = False)
+    except:
+        df.to_csv('Open.csv',index = False)
+
+    nMarkets=CLOSE.shape[1]
+
+    periodLonger=200
+    periodShorter=40
+
+    # Calculate Simple Moving Average (SMA)
+    smaLongerPeriod=numpy.nansum(CLOSE[-periodLonger:,:],axis=0)/periodLonger
+    smaShorterPeriod=numpy.nansum(CLOSE[-periodShorter:,:],axis=0)/periodShorter
+
+    longEquity= smaShorterPeriod > smaLongerPeriod
+    shortEquity= ~longEquity
+
+    pos=numpy.zeros(nMarkets)
+    pos[longEquity]=1
+    pos[shortEquity]=-1
+
+    weights = pos/numpy.nansum(abs(pos))
+
+
+    return weights, settings
+
+
+def mySettings():
+    ''' Define your trading system settings here '''
+
+    settings= {}
+
+    # S&P 100 stocks
+    # settings['markets']=['CASH','AAPL','ABBV','ABT','ACN','AEP','AIG','ALL',
+    # 'AMGN','AMZN','APA','APC','AXP','BA','BAC','BAX','BK','BMY','BRKB','C',
+    # 'CAT','CL','CMCSA','COF','COP','COST','CSCO','CVS','CVX','DD','DIS','DOW',
+    # 'DVN','EBAY','EMC','EMR','EXC','F','FB','FCX','FDX','FOXA','GD','GE',
+    # 'GILD','GM','GOOGL','GS','HAL','HD','HON','HPQ','IBM','INTC','JNJ','JPM',
+    # 'KO','LLY','LMT','LOW','MA','MCD','MDLZ','MDT','MET','MMM','MO','MON',
+    # 'MRK','MS','MSFT','NKE','NOV','NSC','ORCL','OXY','PEP','PFE','PG','PM',
+    # 'QCOM','RTN','SBUX','SLB','SO','SPG','T','TGT','TWX','TXN','UNH','UNP',
+    # 'UPS','USB','UTX','V','VZ','WAG','WFC','WMT','XOM']
+
+    # Futures Contracts
+
+    settings['markets']  = ['CASH','F_AD','F_BO','F_BP','F_C','F_CC','F_CD','F_CL','F_CT','F_DX','F_EC',
+                            'F_ED','F_ES','F_FC','F_FV','F_GC','F_HG','F_HO','F_JY','F_KC','F_LB','F_LC',
+                            'F_LN','F_MD','F_MP','F_NG','F_NQ','F_NR','F_O','F_OJ','F_PA','F_PL','F_RB',
+                            'F_RU','F_S','F_SB','F_SF','F_SI','F_SM','F_TU','F_TY','F_US','F_W','F_XX',
+                            'F_YM','F_AX','F_CA','F_DT','F_UB','F_UZ','F_GS','F_LX','F_SS','F_DL','F_ZQ',
+                            'F_VX','F_AE','F_BG','F_BC','F_LU','F_DM','F_AH','F_CF','F_DZ','F_FB','F_FL',
+                            'F_FM','F_FP','F_FY','F_GX','F_HP','F_LR','F_LQ','F_ND','F_NY','F_PQ','F_RR',
+                            'F_RF','F_RP','F_RY','F_SH','F_SX','F_TR','F_EB','F_VF','F_VT','F_VW','F_GD','F_F',
+                            'AAPL', 'MMM', 'ABT', 'ABBV', 'ACN', 'ALL', 'MO', 'AMZN', 'AEP', 'AXP', 'AIG', 'AMGN',
+                            'APC', 'APA', 'AAPL', 'T', 'BAC', 'BK', 'BAX', 'BRK.B', 'BA', 'BMY', 'COF', 'CAT', 'CVX',
+                            'CSCO', 'C', 'KO', 'CL', 'CMCSA', 'COP', 'COST', 'CVS', 'DVN', 'DOW', 'DD', 'EBAY', 'EMC',
+                            'EMR', 'EXC', 'XOM', 'FB', 'FDX', 'F', 'FCX', 'GD', 'GE', 'GM', 'GILD', 'GS', 'GOOG',
+                            'GOOGL', 'HAL', 'HPQ', 'HD', 'HON', 'INTC', 'IBM', 'JNJ', 'JPM', 'LLY', 'LMT', 'LOW', 'MA',
+                            'MCD', 'MDT', 'MRK', 'MET', 'MSFT', 'MDLZ', 'MON', 'MS', 'NOV', 'NKE', 'NSC', 'OXY', 'ORCL',
+                            'PEP', 'PFE', 'PM', 'PG', 'QCOM', 'RTN', 'SLB', 'SPG', 'SO', 'SBUX', 'TGT', 'TXN', 'TWX',
+                            'FOXA', 'USB', 'UNP', 'UPS', 'UTX', 'UNH', 'VZ', 'V', 'WBA', 'WMT', 'DIS', 'WFC', 'A', 'AA',
+                            'ABC', 'ACE', 'ADBE', 'ADI', 'ADM', 'ADP', 'ADS', 'ADSK', 'ADT', 'AEE', 'AEP', 'AES', 'AET',
+                            'AFL', 'AGN', 'AIV', 'AIZ', 'AKAM', 'ALLE', 'ALTR', 'ALXN', 'AMAT', 'AME', 'AMG', 'AMP',
+                            'AMT', 'AN', 'AON', 'APD', 'APH', 'ARG', 'ATI', 'AVB', 'AVGO', 'AVP', 'AVY', 'AZO', 'BBBY',
+                            'BBT', 'BBY', 'BCR', 'BDX', 'BEN', 'BF.B', 'BHI', 'BLK', 'BLL', 'BMS', 'BRCM', 'BSX', 'BWA',
+                            'BXP', 'CA', 'CAG', 'CAH', 'CAM', 'CB', 'CBG', 'CBS', 'CCE', 'CCI', 'CCL', 'CELG', 'CERN',
+                            'CF', 'CHK', 'CHRW', 'CI', 'CINF', 'CLX', 'CMA', 'CME', 'CMG', 'CMI', 'CMS', 'CNP', 'CNX',
+                            'COG', 'COH', 'COL', 'CPB', 'CRM', 'CSC', 'CSX', 'CTAS', 'CTL', 'CTSH', 'CTXS', 'CVC', 'D',
+                            'DAL', 'DE', 'DFS', 'DG', 'DGX', 'DHI', 'DHR', 'DISCA', 'DISCK', 'DLPH', 'DLTR', 'DNB',
+                            'DNR', 'DO', 'DOV', 'DPS', 'DRI', 'DTE', 'DUK', 'DVA', 'EA', 'ECL', 'ED', 'EFX', 'EIX',
+                            'EL', 'EMN', 'EOG', 'EQR', 'EQT', 'ESRX', 'ESS', 'ESV', 'ETFC', 'ETN', 'ETR', 'EW', 'EXPD',
+                            'EXPE', 'FAST', 'FE', 'FFIV', 'FIS', 'FISV', 'FITB', 'FLIR', 'FLR', 'FLS', 'FMC', 'FOSL',
+                            'FSLR', 'FTI', 'FTR', 'GAS', 'GCI', 'GGP', 'GIS', 'GLW', 'GMCR', 'GME', 'GNW', 'GPC', 'GPS',
+                            'GRMN', 'GT', 'GWW', 'HAR', 'HAS', 'HBAN', 'HCBK', 'HCN', 'HCP', 'HES', 'HIG', 'HOG', 'HOT',
+                            'HP', 'HRB', 'HRL', 'HRS', 'HST', 'HSY', 'HUM', 'ICE', 'IFF', 'INTU', 'IP', 'IPG', 'IR',
+                            'IRM', 'ISRG', 'ITW', 'IVZ', 'JCI', 'JEC', 'JNPR', 'JOY', 'JWN', 'K', 'KEY', 'KIM', 'KLAC',
+                            'KMB', 'KMI', 'KMX', 'KORS', 'KR', 'KSS', 'KSU', 'L', 'LB', 'LEG', 'LEN', 'LH', 'LLL',
+                            'LLTC', 'LM', 'LNC', 'LRCX', 'LUK', 'LUV', 'LVLT', 'LYB', 'M', 'MAC', 'MAR', 'MAS', 'MAT',
+                            'MCHP', 'MCK', 'MCO', 'MHFI', 'MHK', 'MJN', 'MKC', 'MLM', 'MMC', 'MNK', 'MNST', 'MOS',
+                            'MPC', 'MRO', 'MSI', 'MTB', 'MU', 'MUR', 'MYL', 'NAVI', 'NBL', 'NBR', 'NDAQ', 'NE', 'NEE',
+                            'NEM', 'NFLX', 'NFX', 'NI', 'NLSN', 'NOC', 'NRG', 'NTAP', 'NTRS', 'NUE', 'NVDA', 'NWL',
+                            'NWSA', 'OI', 'OKE', 'OMC', 'ORLY', 'PAYX', 'PBCT', 'PBI', 'PCAR', 'PCG', 'PCL', 'PCLN',
+                            'PCP', 'PDCO', 'PEG', 'PFG', 'PGR', 'PH', 'PHM', 'PKI', 'PLD', 'PNC', 'PNR', 'PNW', 'POM',
+                            'PPG', 'PPL', 'PRGO', 'PRU', 'PSA', 'PSX', 'PVH', 'PWR', 'PX', 'PXD', 'QEP', 'R', 'RAI',
+                            'REGN', 'RF', 'RHI', 'RHT', 'RIG', 'RL', 'ROK', 'ROP', 'ROST', 'RRC', 'RSG', 'SCG', 'SCHW',
+                            'SE', 'SEE', 'SHW', 'SJM', 'SNA', 'SNDK', 'SNI', 'SPLS', 'SRCL', 'SRE', 'STI', 'STJ', 'STT',
+                            'STX', 'STZ', 'SWK', 'SWN', 'SYK', 'SYMC', 'SYY', 'TAP', 'TDC', 'TE', 'TEL', 'THC', 'TIF',
+                            'TJX', 'TMK', 'TMO', 'TRIP', 'TROW', 'TRV', 'TSCO', 'TSN', 'TSO', 'TSS', 'TWC', 'TXT',
+                            'TYC', 'UA', 'UHS', 'UNM', 'URBN', 'URI', 'VAR', 'VFC', 'VIAB', 'VLO', 'VMC', 'VNO', 'VRSN',
+                            'VRTX', 'VTR', 'WAT', 'WDC', 'WEC', 'WFM', 'WHR', 'WIN', 'WM', 'WMB', 'WU', 'WY', 'WYN',
+                            'WYNN', 'XEC', 'XEL', 'XL', 'XLNX', 'XRAY', 'XRX', 'XYL', 'YHOO', 'YUM', 'ZION', 'ZTS']
+
+    # settings['beginInSample'] = '20120506'
+    # settings['endInSample'] = '20150506'
+    settings['lookback']= 2520
+    settings['budget']= 10**6
+    settings['slippage']= 0.05
+
+    return settings
+
+# Evaluate trading system defined in current file.
+if __name__ == '__main__':
+    import quantiacsToolbox
+    results = quantiacsToolbox.runts(__file__)
